074ceb92b505069623920807a9be03f302542bdc
thongnv <vanthong07012002@gmail.com>
2025-05-06 12:57:49 +0700

    feat: add ArgoCD Helm release and wait resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -168,3 +168,35 @@ data "kubernetes_service" "nginx_ingress" {
   }
 }
 
+
+resource "helm_release" "argocd" {
+  provider         = helm.with_config
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "7.6.0"
+  namespace        = "argocd"
+  create_namespace = true
+  timeout          = 600
+  wait             = true
+
+  values = [
+    file("${abspath(path.root)}/../helm/argocd/values.yaml")
+  ]
+
+  depends_on = [helm_release.nginx_ingress]
+}
+
+# wait for argocd server and redis to be ready
+resource "null_resource" "wait_for_argocd" {
+  depends_on = [
+    helm_release.argocd
+  ]
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command = <<-EOC
+      kubectl --kubeconfig=${abspath(path.root)}/modules/compute/kubeconfig -n argocd wait --for=condition=Available --timeout=300s deployment/argocd-server && \
+      kubectl --kubeconfig=${abspath(path.root)}/modules/compute/kubeconfig -n argocd wait --for=condition=Ready --timeout=300s pod -l app.kubernetes.io/name=argocd-redis
+    EOC
+  }
+}
