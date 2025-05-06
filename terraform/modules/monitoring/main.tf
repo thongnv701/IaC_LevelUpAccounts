b49af83155abf142ff20d4f3d7d366f1e7b731de
thongnv <vanthong07012002@gmail.com>
2025-05-06 17:17:30 +0700

    fix: remove argocd helm release and wait resource from monitoring module

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -1,32 +1,3 @@
-resource "helm_release" "argocd" {
-  provider         = helm.with_config
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  version          = "7.6.0"
-  namespace        = "argocd"
-  create_namespace = true
-  timeout          = 600
-  wait             = true
-
-  values = [
-    file("${abspath(path.root)}/../helm/argocd/values.yaml")
-  ]
-}
-
-# wait for argocd server and redis to be ready
-resource "null_resource" "wait_for_argocd" {
-  depends_on = [
-    helm_release.argocd
-  ]
-  provisioner "local-exec" {
-    interpreter = ["bash", "-c"]
-    command = <<-EOC
-      kubectl --kubeconfig=${abspath(path.root)}/modules/compute/kubeconfig -n argocd wait --for=condition=Available --timeout=300s deployment/argocd-server && \
-      kubectl --kubeconfig=${abspath(path.root)}/modules/compute/kubeconfig -n argocd wait --for=condition=Ready --timeout=300s pod -l app.kubernetes.io/name=argocd-redis
-    EOC
-  }
-}
 
 # data "aws_route53_zone" "main" {
 #   name = "thongit.space."
