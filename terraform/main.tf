4ffe51671cf12b88c25d40dd2271a498141abca9
thongnv <vanthong07012002@gmail.com>
2025-05-06 07:28:53 +0700

    fix: add dependency on nginx_ingress service for Route53 record

--- terraform/main.tf
+++ terraform/main.tf
@@ -180,6 +180,7 @@ data "aws_route53_zone" "main" {
 }
 
 resource "aws_route53_record" "argocd" {
+  depends_on = [kubernetes_service.nginx_ingress]
   count   = can(data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname) && data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname != "" ? 1 : 0
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "argocd.thongit.space"
