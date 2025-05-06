7ed85d126c212fa7331079eb2889f9fb58990f03
thongnv <vanthong07012002@gmail.com>
2025-05-06 07:39:02 +0700

    fix: uncomment Terraform Plan step and update apply command to use tfplan

--- terraform/main.tf
+++ terraform/main.tf
@@ -179,12 +179,12 @@ data "aws_route53_zone" "main" {
   name = "thongit.space."
 }
 
-resource "aws_route53_record" "argocd" {
-  depends_on = [data.kubernetes_service.nginx_ingress]
-  count   = can(data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname) && data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname != "" ? 1 : 0
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "argocd.thongit.space"
-  type    = "CNAME"
-  ttl     = 300
-  records = [data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname]
-}
+# resource "aws_route53_record" "argocd" {
+#   depends_on = [data.kubernetes_service.nginx_ingress]
+#   count   = can(data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname) && data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname != "" ? 1 : 0
+#   zone_id = data.aws_route53_zone.main.zone_id
+#   name    = "argocd.thongit.space"
+#   type    = "CNAME"
+#   ttl     = 300
+#   records = [data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname]
+# }
