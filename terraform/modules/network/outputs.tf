2f975c5aea0a71a8bcbe8b7aaada8b426b05dcc8
thongnv <vanthong07012002@gmail.com>
2025-05-07 13:09:28 +0700

    fix: restructure outputs in outputs.tf for clarity and consistency

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,3 +1,15 @@
-output "vpc_id" { value = aws_vpc.k3s_vpc.id }
-output "subnet_ids" { value = [aws_subnet.k3s_subnet_1.id, aws_subnet.k3s_subnet_2.id] }
-output "security_group_id" { value = aws_security_group.k3s_security_group.id }
+output "vpc_id" {
+  value = aws_vpc.k3s_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.k3s_subnet_1.id  # Using the first subnet for the compute module
+}
+
+output "subnet_ids" {
+  value = [aws_subnet.k3s_subnet_1.id, aws_subnet.k3s_subnet_2.id]
+}
+
+output "security_group_id" {
+  value = aws_security_group.k3s_security_group.id
+}
