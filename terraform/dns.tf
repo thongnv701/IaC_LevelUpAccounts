bcd02bb7b4ddfec601a7e05305c35e71d6745e12
thongnv <vanthong07012002@gmail.com>
2025-05-08 22:26:24 +0700

    feat: implement complete infrastructure deployment workflow with Terraform, Nginx Ingress, Cert Manager, and DNS management

---
+++ terraform/dns.tf
@@ -0,0 +1,53 @@
+# Route 53 DNS Management
+
+# Get the hosted zone for thongit.space
+data "aws_route53_zone" "main" {
+  name = "thongit.space"
+}
+
+# Get the public IPs of the EC2 instances
+data "aws_instances" "k3s_instances" {
+  filter {
+    name   = "tag:Name"
+    values = ["*k3s*"]
+  }
+
+  filter {
+    name   = "instance-state-name"
+    values = ["running"]
+  }
+}
+
+locals {
+  # Extract all available public IPs (excluding empty ones)
+  public_ips = [
+    for instance in data.aws_instances.k3s_instances.public_ips : 
+    instance if instance != null && instance != ""
+  ]
+  
+  # Define all the domains we want to manage
+  managed_domains = [
+    "argocd.thongit.space",
+    "grafana.thongit.space"
+  ]
+}
+
+# Create A records for all managed domains
+resource "aws_route53_record" "service_records" {
+  for_each = toset(local.managed_domains)
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value
+  type    = "A"
+  ttl     = "300"
+  records = local.public_ips
+}
+
+# Output the created DNS records
+output "dns_records" {
+  value = {
+    for domain in local.managed_domains :
+    domain => "${domain} -> ${join(", ", local.public_ips)}"
+  }
+  description = "DNS records created in Route 53"
+} 
--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -5,6 +5,6 @@ variable "subnet_id" {}
 variable "security_group_id" {}
 variable "master_user_data" {}
 variable "worker_user_data" {}
-variable "worker_count" { default = 1}
+variable "worker_count" { default = 2}
 variable "kubeconfig_fetch_script" {}
 variable "private_key_content" {}
