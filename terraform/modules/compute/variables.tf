5a9b043843802137f50e057be380af0cb4a6d7b7
thongnv <vanthong07012002@gmail.com>
2025-05-13 10:08:34 +0700

    refactor: update default worker_count in compute module from 2 to 1 for optimized resource allocation

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -6,7 +6,7 @@ variable "subnet_id" {}
 variable "security_group_id" {}
 variable "master_user_data" {}
 variable "worker_user_data" {}
-variable "worker_count" { default = 2}
+variable "worker_count" { default = 1}
 variable "kubeconfig_fetch_script" {}
 variable "private_key_content" {}
 variable "http_target_group_arn" {
