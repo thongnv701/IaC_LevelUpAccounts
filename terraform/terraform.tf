d86196f24fcc8f6509637b7eed2531532266f0aa
thongnv <vanthong07012002@gmail.com>
2025-07-09 11:47:14 +0700

    refactor: remove AWS provider block from main.tf, delete providers.tf, and update terraform.tf to specify required providers and versions for improved dependency management

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,33 @@
 terraform {
+  required_version = ">= 1.3.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "my-terraform-backup-0701"
     key            = "levelupaccounts/terraform.tfstate"
